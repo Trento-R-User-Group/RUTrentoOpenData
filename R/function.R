2b74679ede30161a22b120ee6dd63c19433abfcd
Daniele Andreis <danieleandreis@MBP-di-Daniele.station>
2016-12-20 20:56:23 +0100

    copy template

---
+++ R/function.R
@@ -0,0 +1,33 @@
+# TODO: Add comment
+# 
+# Author: ecor
+###############################################################################
+NULL
+#' 
+#' Something about the function ....
+#' 
+#' @param x first argument
+#' @param ... further arguments
+#' 
+#' @export 
+#' 
+#' @importFrom stringr str_replace
+#'
+#' @examples 
+#' 
+#' ll <- funzione()
+#' 
+#' 
+#' 
+#' 
+#' 
+
+
+funzione <- function (x=NULL,...) {	
+	
+	out <- x
+
+	return(out)
+	
+
+}	
---
+++ README.md
@@ -0,0 +1,4 @@
+# RPackageTemplate
+This is a first draf for an R package template. 
+
+This project will be "via-via" created updated and will be finished in the next R Trento Users' meeting, in December 2016.
