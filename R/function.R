60987a84b5584b23c8becc00edc5aabb3d456e07
unknown <pr43569@PAT29415.ad.provincia.tn.it>
2016-12-29 17:08:43 +0100

    first functions, test menus

--- R/function.R
+++ R/function.R
@@ -1,21 +1,23 @@
 # TODO: Add comment
 # 
-# Author: ecor
+# Author: Trento R User Group
 ###############################################################################
 NULL
 #' 
-#' Something about the function ....
+#' Main search function
 #' 
-#' @param x first argument
+#' This is the main search function and will return the result of the query
+#' from dati.trentino
+#' 
+#' @param q query
 #' @param ... further arguments
 #' 
-#' @export 
+#' @return 
 #' 
-#' @importFrom stringr str_replace
 #'
 #' @examples 
 #' 
-#' ll <- funzione()
+#' ll <- search_query('nati')
 #' 
 #' 
 #' 
@@ -23,11 +25,58 @@ NULL
 #' 
 
 
-funzione <- function (x=NULL,...) {	
-	
-	out <- x
-
-	return(out)
-	
-
+search_query <- function (q = NULL,...) {
+    
+    ckanr::ckanr_setup('dati.trentino.it')
+    
+    if (is.null(q) | q == '') {
+        message('Empty query, please ask something')
+        return(NULL)
+    } else {
+        res <- ckanr::package_search(q = q,
+                                     as = 'table',
+                                     rows = 100)$results
+        return(res)
+    }
+    
+    
 }	
+
+#'
+#' Interactive menu to download a package
+#' 
+#' @param q query
+#'
+
+packages_menu <- function(q = NULL, sel = NULL ) {
+    ckanr::ckanr_setup('dati.trentino.it')
+    r <- search_query(q)
+    if (!is.null(r) & is.null(sel)) {
+        choices <- r$title
+        c <- menu(choices, 
+                  title = 'The query returned these datasets, 
+                  which one do you want to load?')
+        resources <- r[c, 'resources'][[1]]
+        return(resources)
+    }
+}
+
+#'
+#' Interactive menu to download a resource
+#' 
+#' @param resources A data.frame as returned by \code{packages_menu}
+#'
+
+
+resources_menu <- function(resources = NULL ) {
+    choices <- recources$name
+    if (!is.null(resources))
+    c <- menu(choices,
+              title = 'These are the available resources,
+                  which one do you want?')
+    resources[c, 'url']
+    result <- read.csv2(resources[c, 'url'])
+    return(result)
+}
+
+
