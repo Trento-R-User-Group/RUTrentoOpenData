d279930b247cf3657acc26890c55501ffc672e37
giackgamba <giackgamba@gmail.com>
2017-01-18 05:58:39 +0100

    Several fixes
    globals.R
    trentino() streamlined
    search_query() now check the query insted of main func
    
    packages.R
    pack_default() renamed, overwrite default values
    menu_packages() remove unused res_sel, added warning
    
    resource.R
    select_resource() if pack not picked by apply default values
    download_resource() added sep params (from '...')

--- R/globals.R
+++ R/globals.R
@@ -1,49 +1,56 @@
-#' 
+#'
 #' Main function / entry point
-#' 
+#'
 #' This is the main function and then only that should be invoked, at least for now
-#' 
+#'
 #' @param q A text input to search and download data
-#' @param rows Number of rows to be returned from the query, DEFAULT=100 (TODO: Possibily add a warning/message when max is reached)
 #' @param pack_sel Package selection. Custom parameter to override, force or set default package
 #' @param res_sel Resource selection. Custom parameter to override, force or set default resource
-#' 
+#' @param rows Number of rows to be returned from the query
+#' @param ... Additional parameters to further customize behaviour
+#'
 #' @export
-#' 
+#'
 #' @return A dataset if everything goes ok, otherwise...(TODO)
-#' 
-trentino <- function(q, rows = 100, pack_sel = NULL, res_sel = NULL) {
-    #IMPORTANT
-    ckanr::ckanr_setup("dati.trentino.it")
+#'
+trentino <- function(q,
+             pack_sel = NULL,
+             res_sel = NULL,
+             rows = 100,
+             ...) {
+        #IMPORTANT
+        ckanr::ckanr_setup("dati.trentino.it")
 
-    if (is.null(q) | q == "") {
-        message("Empty query, please ask something")
-        return(NULL)
+        s <- search_query(q, rows = rows)
+        p <- menu_packages(s = s,
+                           rows = rows,
+                           pack_sel = pack_sel)
+        r <- select_resource(pack = p,
+                             res_sel = res_sel)
+        download_resource(r, ...)
+        # Should return a data.frame
     }
-    p <- menu_packages(q = q,
-                       rows = rows,
-                       pack_sel = pack_sel,
-                       res_sel = res_sel)
-    r <- select_resource(pack = p,
-                         res_sel = res_sel)
-    download_resource(r)
-    # Should return a data.frame
-}
 
 
-#' 
+#'
 #' Search function
-#' 
+#'
 #' This function search the portal for the specified query and return a data.frame containing the related packages
-#' 
+#'
 #' @param q query
 #' @param rows Number of rows to be returned from the query, DEFAULT=100 (TODO: Possibily add a warning/message when max is reached)
-#' 
+#'
 #' @return A data.frame as returned from ckanr::package_search
-#' 
+#'
 search_query <- function (q, rows = 100) {
+    if (is.null(q) | q == "") {
+        stop("Empty query, please ask something")
+        return(NULL)
+    }
+
     res <- ckanr::package_search(q = q,
                                  rows = rows,
                                  as = "table")
+
     res <- res$results
 }
--- R/packages.R
+++ R/packages.R
@@ -2,27 +2,48 @@
 #' Packages Menu
 #'
 #' Interactive menu to download a package
-#' 
-#' @param q query
+#'
+#' @param s query
 #' @param rows Number of rows to be returned in the menu, DEFAULT=100 (TODO: Possibily add a warning/message if rows is reached)
 #' @param pack_sel Override Package selection
-#' @param res_sel Override Resource selection
-#' 
+#'
 #' @return The selected Package
 #'
-menu_packages <- function(q = NULL,
+menu_packages <- function(s,
                           rows = 100,
-                          pack_sel = NULL,
-                          res_sel = NULL) {
-    r <- search_query(q, rows = rows)
-    if (!is.null(r)) {
+                          pack_sel = NULL) {
+    if (!is.null(s)) {
         if (is.null(pack_sel)) {
-            choices <- r$title
+            choices <- s$title
             pack_sel <- menu(choices,
-                        title = "The query returned these datasets,
-                        which one do you want to load?")
-        }
-        pack <- r[pack_sel, ]
+                             title = "The query returned these datasets,
+                             which one do you want to load?")
+            if (nrow(s) == rows) {
+                message(
+                    "There are probably more datasets than showed,
+                    use the rows parameter to increase the limit"
+                )
+            }
+            }
+        pack <- s[pack_sel,]
         return(pack)
+        }
     }
+#'
+#' Organization default resource
+#'
+#' If we know the structure of a organization tipical package, we can attempt to download it automatically
+#'
+#' @param pack Package df as returned by menu_packages
+#' @param res Resource options (RENAME - REFACTOR)
+#'
+#' @return The automatically selected Resource
+#'
+pack_default <- function(pack, res) {
+    if (pack$organization$title == "ISPAT") {
+        res <- list(res_sel = 2,
+                    sep = ";")
+    }
+    
+    return(res)
 }
